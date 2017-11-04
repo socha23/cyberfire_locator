4c4dc93b0d708684f64ad4952a84a19485c0201f
socha23 <socha23@gmail.com>
2017-11-05 00:12:45 +0100

    repository init

---
+++ app/src/main/java/pl/socha23/cyberfirelocator/MainActivity.java
@@ -0,0 +1,23 @@
+package pl.socha23.cyberfirelocator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getStatusView().setText("YOOOOOO");
+
+    }
+
+    private TextView getStatusView() {
+        return (TextView)findViewById(R.id.label_status);
+    }
+}
