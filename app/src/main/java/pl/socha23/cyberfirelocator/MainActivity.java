8aeb669da2f99676ba4a3ea9026fe430652df2c5
socha23 <socha23@gmail.com>
2017-11-05 14:52:44 +0100

    basic preferences activity

--- app/src/main/java/pl/socha23/cyberfirelocator/MainActivity.java
+++ app/src/main/java/pl/socha23/cyberfirelocator/MainActivity.java
@@ -1,23 +1,95 @@
 package pl.socha23.cyberfirelocator;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient googleApiClient;
+    private Location lastLocation;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        getStatusView().setText("YOOOOOO");
+        initGoogleApiClient();
+
+        setContentView(R.layout.activity_main);
 
     }
 
+    private void initGoogleApiClient() {
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
     private TextView getStatusView() {
-        return (TextView)findViewById(R.id.label_status);
+        return (TextView) findViewById(R.id.label_status);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            throw new RuntimeException("No permissions");
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            //return;
+        }
+        lastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                googleApiClient);
+        if (lastLocation != null) {
+            getStatusView().setText(String.valueOf(lastLocation.getLatitude()) + " x " + String.valueOf(lastLocation.getLongitude()));
+        } else {
+            getStatusView().setText("Couldn't retrieve location");
+            throw new RuntimeException("Couldnt retrieve locayion");
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        throw new RuntimeException("Connection to google services suspendeds");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, this.getResources().getString(R.string.toast_connection_failed), Toast.LENGTH_LONG).show();
+    }
+
+    public void onPreferencesButtonClick(View view) {
+        startActivity(PreferencesActivity.createIntent(this));
     }
 }
---
+++ app/src/main/java/pl/socha23/cyberfirelocator/PreferencesActivity.java
@@ -0,0 +1,32 @@
+package pl.socha23.cyberfirelocator;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class PreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.preferences_container, new PreferencesFragment())
+                .commit();
+    }
+
+    public static class PreferencesFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+    public static Intent createIntent(Context ctx) {
+        return new Intent(ctx, PreferencesActivity.class);
+    }
+}
